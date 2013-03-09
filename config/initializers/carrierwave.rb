6625b30049afb2fd0552e0c06fee2076ea1fb4ab
Ian Smith <ianhenrysmith@gmail.com>
2013-03-09 15:40:58 -0700

    IS: tryna fix s3 upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+    :region                 => 'us-east-1'
   }
   config.fog_directory  = ENV['AWS_BUCKET']
   config.fog_public     = true
