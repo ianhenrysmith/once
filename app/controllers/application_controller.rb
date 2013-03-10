d5c175841e5b15d8a440211a6ab1e8ed2ff4fe01
Ian Smith <ianhenrysmith@gmail.com>
2013-03-09 19:06:07 -0700

    IS: dry out layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  layout "application"
+  
   def index
   end
   
+  private
+  
+  def layout
+    # only turn it off for login pages:
+    # is_a?(Devise::SessionsController) ? false : "application"
+    # or turn layout off for every devise controller:
+    devise_controller? && "application"
+  end
 end
