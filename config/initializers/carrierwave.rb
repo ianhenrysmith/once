0413abc3cc45ca4cf797fa2c1b4d7d625ca73976
Ian Smith <ianhenrysmith@gmail.com>
2013-03-11 09:24:41 -0600

    IS: assets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
     :region                 => 'us-east-1'
   }
-  config.fog_host       = "http://#{ENV['AWS_BUCKET']}.s3.amazonaws.com"
+  config.asset_host     = "http://#{ENV['AWS_BUCKET']}.s3.amazonaws.com"
   config.fog_directory  = ENV['AWS_BUCKET']
   config.fog_public     = true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
