1cc282d1e16a63841d5df86e0b2d9285b446dec1
Ian Smith <ianhenrysmith@gmail.com>
2013-03-20 08:40:54 -0600

    IS: little layout changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -68,12 +68,19 @@ class Post
   
   def as_json(options={})
     # should also cache this based on user id, and figure out way to expire old items
-    Rails.cache.fetch("post_#{id}_#{updated_at}_as_json", expires: 1.hour) do
+    Rails.cache.fetch("post_#{id}_#{updated_at}_as_json", expires: 1.day) do
       result = super(options)
       result["id"] = id.to_s
-      result["created_string"] = created_at.strftime("%D") if created_at
+      result["created_string"] = created_at.strftime("%m/%d/%y") if created_at
       result["tweet_embed_code"] = CGI::escape(tweet_embed_code) if tweet_embed_code
-    
+      
+      if u = user
+        result["creator_name"] = u.name
+        result["creator_path"] = "/users/#{u.id}"
+        result["creator_bb_path"] = "/#/user/#{u.id}"
+        result["creator_avatar_url"] = u.asset_url
+      end
+      
       result
     end
   end
